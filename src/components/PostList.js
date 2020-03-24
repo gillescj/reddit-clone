f1f419bc9aea443cf56237f849c0c939f03fed48
Colin Gillespie <ratchetandcolin@gmail.com>
2020-03-24 19:23:11 -0400

    Added Sorting Options

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,14 +6,56 @@ import Post from './Post';
 import Pagination from './Pagination';
 
 const PostList = () => {
-    const { posts } = useContext(StateContext);
+    const { posts, setSettings, setPagination } = useContext(StateContext);
 
     const renderedPostList = posts.map(post => {
         return <Post key={post.data.id} post={post} />;
     });
 
+    const handleSortMenuOptionClick = option => {
+        setPagination(previousPagination => {
+            return {
+                ...previousPagination,
+                pageNumber: 1,
+                query: ''
+            };
+        });
+        setSettings(previousSettings => {
+            return {
+                ...previousSettings,
+                orderBy: option
+            };
+        });
+    };
+
     return (
         <div className="post-list">
+            <div className="sort-menu">
+                <span
+                    onClick={() => handleSortMenuOptionClick('hot')}
+                    className="sort-menu-option"
+                >
+                    hot
+                </span>
+                <span
+                    onClick={() => handleSortMenuOptionClick('new')}
+                    className="sort-menu-option"
+                >
+                    new
+                </span>
+                <span
+                    onClick={() => handleSortMenuOptionClick('top')}
+                    className="sort-menu-option"
+                >
+                    top
+                </span>
+                <span
+                    onClick={() => handleSortMenuOptionClick('rising')}
+                    className="sort-menu-option"
+                >
+                    rising
+                </span>
+            </div>
             {renderedPostList}
             <Pagination />
         </div>
