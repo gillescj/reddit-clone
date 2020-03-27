f7fdde1f63b9e2c26a2c6db1431b55d2c53c91eb
Colin Gillespie <ratchetandcolin@gmail.com>
2020-03-27 03:18:40 -0400

    Trending Subreddit api url

--- src/components/ExtraInfo.js
+++ src/components/ExtraInfo.js
@@ -3,6 +3,7 @@ import '../styles/ExtraInfo.scss';
 import React from 'react';
 
 const ExtraInfo = () => {
+    // https://www.reddit.com/subreddits/popular.json?limit=10
     return <div className="extra-info">Extra Info</div>;
 };
 
