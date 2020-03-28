0972f2e74302f9e2ca55c419aa38f56e3c57d6b4
Colin Gillespie <ratchetandcolin@gmail.com>
2020-03-28 17:13:27 -0400

    Active colour on selected sort menu item

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,7 +6,7 @@ import Post from './Post';
 import Pagination from './Pagination';
 
 const PostList = () => {
-    const { posts, setSettings, setPagination } = useContext(StateContext);
+    const { posts, settings, setSettings, setPagination } = useContext(StateContext);
 
     const renderedPostList = posts.map(post => {
         return <Post key={post.data.id} post={post} />;
@@ -33,25 +33,33 @@ const PostList = () => {
             <div className="sort-menu">
                 <span
                     onClick={() => handleSortMenuOptionClick('hot')}
-                    className="sort-menu-option"
+                    className={`sort-menu-option ${
+                        settings.orderBy === 'hot' ? 'sort-menu-option-selected' : ''
+                    }`}
                 >
                     hot
                 </span>
                 <span
                     onClick={() => handleSortMenuOptionClick('new')}
-                    className="sort-menu-option"
+                    className={`sort-menu-option ${
+                        settings.orderBy === 'new' ? 'sort-menu-option-selected' : ''
+                    }`}
                 >
                     new
                 </span>
                 <span
                     onClick={() => handleSortMenuOptionClick('top')}
-                    className="sort-menu-option"
+                    className={`sort-menu-option ${
+                        settings.orderBy === 'top' ? 'sort-menu-option-selected' : ''
+                    }`}
                 >
                     top
                 </span>
                 <span
                     onClick={() => handleSortMenuOptionClick('rising')}
-                    className="sort-menu-option"
+                    className={`sort-menu-option ${
+                        settings.orderBy === 'rising' ? 'sort-menu-option-selected' : ''
+                    }`}
                 >
                     rising
                 </span>
