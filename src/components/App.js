7d229e7f139f45f53e28e4716a3a7c8237b361d6
Colin Gillespie <ratchetandcolin@gmail.com>
2020-04-04 03:39:06 -0400

    Moved front page useEffects to MainContainer.js Moved from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import '../styles/App.scss';
 
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { HashRouter } from 'react-router-dom';
 import StateContext from './StateContext';
 import Header from './Header';
-import reddit from '../apis/reddit';
 import MainContainer from './MainContainer';
 
 const App = () => {
@@ -47,38 +46,6 @@ const App = () => {
             setLoading
         ]
     );
-
-    useEffect(() => {
-        setUrl(
-            `${settings.page}${settings.orderBy}.json?limit=${settings.limit}&${pagination.query}&g=GLOBAL`
-        );
-    }, [settings.orderBy, settings.limit, pagination.query]);
-
-    useEffect(() => {
-        window.scrollTo(0, 0);
-        const fetchData = async () => {
-            setLoading(true);
-            console.log(url);
-
-            const response = await reddit.get(url);
-            console.log(response);
-
-            setPagination(previousPagination => {
-                return {
-                    ...previousPagination,
-                    before: response.data.data.children[0].data.name,
-                    after:
-                        response.data.data.children[
-                            response.data.data.children.length - 1
-                        ].data.name
-                };
-            });
-
-            setPosts(response.data.data.children);
-            setLoading(false);
-        };
-        fetchData();
-    }, [url]);
 
     return (
         <StateContext.Provider value={state}>
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,60 @@
 import '../styles/MainContainer.scss';
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Subreddit from './Subreddit';
 import StateContext from './StateContext';
 import ExtraInfo from './ExtraInfo';
 import Home from './Home';
 import PostDetail from './PostDetail';
+import reddit from '../apis/reddit';
 
 const MainContainer = () => {
-    const { loading } = useContext(StateContext);
+    const {
+        posts,
+        setPosts,
+        pagination,
+        setPagination,
+        settings,
+        setSettings,
+        url,
+        setUrl,
+        loading,
+        setLoading
+    } = useContext(StateContext);
+
+    useEffect(() => {
+        setUrl(
+            `${settings.page}${settings.orderBy}.json?limit=${settings.limit}&${pagination.query}&g=GLOBAL`
+        );
+    }, [settings.orderBy, settings.limit, pagination.query]);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+        const fetchData = async () => {
+            setLoading(true);
+            console.log(url);
+
+            const response = await reddit.get(url);
+            console.log(response);
+
+            setPagination(previousPagination => {
+                return {
+                    ...previousPagination,
+                    before: response.data.data.children[0].data.name,
+                    after:
+                        response.data.data.children[
+                            response.data.data.children.length - 1
+                        ].data.name
+                };
+            });
+
+            setPosts(response.data.data.children);
+            setLoading(false);
+        };
+        fetchData();
+    }, [url]);
+
     return (
         <main className="main-container">
             {loading ? (
