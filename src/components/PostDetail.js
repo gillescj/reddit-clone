5a1027eb08b9bf81e0bc255276d5d287ac2b16d1
Colin Gillespie <ratchetandcolin@gmail.com>
2020-04-10 18:16:02 -0400

    Post Detail now sets settings.subreddit state.

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,14 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import StateContext from './StateContext';
 import reddit from '../apis/reddit';
 import Post from '../components/Post';
 import CommentList from './CommentList';
 
 const PostDetail = ({ match }) => {
+    const { setSettings } = useContext(StateContext);
+
     const [post, setPost] = useState();
     const [comments, setComments] = useState();
     const [postUrl, setpostUrl] = useState(`comments/${match.params.postId}.json`);
 
     useEffect(() => {
+        setSettings((previousSettings) => {
+            return {
+                ...previousSettings,
+                page: match.url,
+                subreddit: match.url.substring(
+                    match.url.indexOf('r/') + 2,
+                    match.url.indexOf('/p/')
+                ),
+            };
+        });
         const fetchData = async () => {
             const response = await reddit.get(postUrl);
             setPost(response.data[0].data.children[0]);
