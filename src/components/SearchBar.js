d80384fb64b2ca72a9ddd72db62fdf2d04703593
Colin Gillespie <ratchetandcolin@gmail.com>
2020-07-06 19:26:18 -0400

    Rename SearchForm into SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,25 @@
-import '../styles/SearchForm.scss';
+import '../styles/SearchBar.scss';
 
 import React, { useState } from 'react';
 
 const SearchForm = () => {
     const [barValue, setBarValue] = useState('');
 
-    const handleSearchBarChange = event => {
+    const handleSearchBarChange = (event) => {
         setBarValue(event.target.value);
     };
 
-    const handleFormSubmit = event => {
+    const handleFormSubmit = (event) => {
         event.preventDefault();
         alert('Search functionality coming soon.');
     };
 
     return (
-        <form onSubmit={handleFormSubmit} className="search-form">
+        <form onSubmit={handleFormSubmit} className="search-bar">
             <input
                 type="text"
                 placeholder="Search"
-                className="search-bar"
+                className="search-bar-input"
                 value={barValue}
                 onChange={handleSearchBarChange}
             />
